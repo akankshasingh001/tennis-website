19d85bdc965dba914ebb7b61d6c3cb33c4ea018a
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-11 18:12:37 -0400

    Profile Model Created

---
+++ Model/Profile.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProfileSchema = new Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
+  image: { type: String },
+  yearsPlayed: { type: Number },
+  profession: { type: String },
+  interests: { type: [String] },
+  favPro: { type: [String] },
+  contactNumber: { type: Number }
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
