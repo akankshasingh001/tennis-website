3a0f27ff0b1dbee34c2aa95dd46ee51e864d8d89
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-12 14:18:01 -0400

    added status in file

--- routes/api/getUserDetails.js
+++ routes/api/getUserDetails.js
@@ -11,7 +11,7 @@ const User = require('../../Model/User');
 router.get('/', auth, async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select('-password');
-    res.json(user);
+    res.status(200).json(user);
   } catch (err) {
     console.log(err.message);
     res.status(500).send('Server Error');
