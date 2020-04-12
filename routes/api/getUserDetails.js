a2a9cc17705048289f434a163076d8a4a91af303
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-12 11:01:24 -0400

    logic created for get user details

---
+++ routes/api/getUserDetails.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+const auth = require('../../middleware/auth');
+const User = require('../../Model/User');
+
+// @route        GET api/getUser
+// @Description  Get user details if authentiacted
+// @access       Public
+
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+module.exports = router;
