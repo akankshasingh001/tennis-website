719638ce3669e7c76edbaa2c486108c0f795fc8b
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-13 10:11:43 -0400

    token set on first page load

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Navigation from './layout/Navigation';
+import Landing from './layout/Landing';
+import Routes from './routing/Routes';
+
 import store from '../store';
+import setAuthToken from '../utils/setAuthToken';
+import loadUserAfterLogin from '../actions/afterLogin';
 
 class App extends Component {
+  componentDidMount() {
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+      store.dispatch(loadUserAfterLogin());
+    }
+  }
   render() {
     return (
       <Provider store={store}>
-        <div>
-          <Navigation />
-        </div>
+        <Router>
+          <div>
+            <Navigation />
+            <Switch>
+              <Route exact path="/" component={Landing}></Route>
+              <Route component={Routes} />
+            </Switch>
+          </div>
+        </Router>
       </Provider>
     );
   }
