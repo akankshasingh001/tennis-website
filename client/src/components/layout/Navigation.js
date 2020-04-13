7ec401f9a01672fc0465478892fed834a00ba0cc
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-13 19:12:24 -0400

    Completed Navigation Component

--- client/src/components/layout/Navigation.js
+++ client/src/components/layout/Navigation.js
@@ -1,13 +1,68 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { logout } from '../../actions/login';
 
-const Navigation = () => {
+const Navigation = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authLinks = (
+    <ul>
+      <li>
+        <Link to="/searchPlayer">
+          <i class="search icon" />
+          <span>Search Player</span>
+        </Link>
+      </li>
+      <li>
+        <Link to="/account">My Account</Link>
+      </li>
+      <li>
+        <Link to="/dashboard">
+          <i className="user icon" />
+          <span>DashBoard</span>
+        </Link>
+      </li>
+      <li>
+        <Link onClick={logout} to="/">
+          <i className="sign out alternate icon" /> <span>Logout</span>
+        </Link>
+      </li>
+    </ul>
+  );
+  const guestLinks = (
+    <ul>
+      <li>
+        <Link to="/aboutus">About Us</Link>
+      </li>
+      <li>
+        <Link to="/register">Register</Link>
+      </li>
+      <li>
+        <Link to="/login">
+          <span>Login</span>
+          <i className=" sign in alternate icon" />
+        </Link>
+      </li>
+    </ul>
+  );
   return (
-    <div>
-      <Link to="/register">Register</Link>
-      <Link to="/login">Login</Link>
-    </div>
+    <nav className="navbar bg-dark">
+      <h1>
+        <Link to="/">
+          <i className="iconify" data-icon="mdi-tennis" />
+          Tennis Junction
+        </Link>
+      </h1>
+      {!loading && (
+        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+      )}
+    </nav>
   );
 };
+const mapStateToProps = state => ({
+  auth: state.auth
+});
 
-export default Navigation;
+export default connect(
+  mapStateToProps,
+  { logout }
+)(Navigation);
