19c962391b39cc03857cf4de94116f4298846cef
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-12 21:50:41 -0400

    connected store to app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import Navigation from './layout/Navigation';
+import store from '../store';
 
 class App extends Component {
   render() {
-    return <div>App</div>;
+    return (
+      <Provider store={store}>
+        <div>
+          <Navigation />
+        </div>
+      </Provider>
+    );
   }
 }
 
