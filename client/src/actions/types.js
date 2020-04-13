6792b0920ccd5a1b3b2bf4294c8d8f12343be28f
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-13 17:45:09 -0400

    added action type for clearProfile

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,6 +1,7 @@
 export const LOG_IN = 'LOG_IN';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const LOG_OUT = 'LOG_OUT';
+export const CLEAR_PROFILE = 'CLEAR_PROFILE';
 export const USER_LOADED = 'USER_LOADED';
 export const AUTH_ERROR = 'AUTH_ERROR';
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
