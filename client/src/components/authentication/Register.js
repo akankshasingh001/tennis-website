f52a47f3c7d35cd8a9de49b41400c0aa90ffcc25
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-13 10:22:42 -0400

    Created Register Component

---
+++ client/src/components/authentication/Register.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+// import { login } from '../../actions/login';
+
+class Register extends Component {
+  render() {
+    return <div>Register</div>;
+  }
+}
+
+export default Register;
