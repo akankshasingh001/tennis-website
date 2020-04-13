b96854eccf0fdda0fc2563da8e05f8727d676aa2
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-13 14:04:49 -0400

    Login Component Completed

--- client/src/components/authentication/Login.js
+++ client/src/components/authentication/Login.js
@@ -1,15 +1,75 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Fragment, useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { login } from '../../actions/login';
 
-class Login extends Component {
-  render() {
-    return <div>Login</div>;
-  }
-}
+const Login = ({ login, isAuthenticated }) => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+  const { email, password } = formData;
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
 
+  const onSubmit = async e => {
+    e.preventDefault();
+    login(email, password);
+  };
+  //Redirect if logged in
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  }
+  return (
+    <Fragment>
+      <div className="ui container">
+        <h1>Sign In</h1>
+        <p>
+          <i className="user icon"></i> Sign Into Your Account
+        </p>
+        <form className="ui form" onSubmit={e => onSubmit(e)}>
+          <div className="field">
+            <label>Email</label>
+            <input
+              type="email"
+              placeholder="Email Address"
+              name="email"
+              value={email}
+              onChange={e => onChange(e)}
+              required
+            />
+          </div>
+          <div className="field">
+            <label>Password</label>
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              minLength="6"
+              value={password}
+              onChange={e => onChange(e)}
+            />
+          </div>
+          <input type="submit" className="ui button primary" value="Login" />
+        </form>
+        <p className="my-1">
+          Don't have an account? <Link to="/register">Sign Up</Link>
+        </p>
+      </div>
+    </Fragment>
+  );
+};
+
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
 export default connect(
-  null,
+  mapStateToProps,
   { login }
 )(Login);
