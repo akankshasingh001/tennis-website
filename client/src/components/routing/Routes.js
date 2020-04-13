0969863db1c6507daea84b184495191c3e6fb5c2
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-13 14:06:06 -0400

    added dashboard route

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from '../authentication/Login';
 import Register from '../authentication/Register';
+import Dashboard from '../dashboard/UserDashboard';
 
 const Routes = () => {
   return (
     <Switch>
       <Route path="/login" exact component={Login} />
       <Route path="/register" exact component={Register} />
+      <Route path="/dashboard" exact component={Dashboard} />
     </Switch>
   );
 };
