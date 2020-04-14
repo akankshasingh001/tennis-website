ea5d435ed925505aa0d8164a57a15940f559f615
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-14 12:04:42 -0400

    added user dashboard action type

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -6,3 +6,4 @@ export const USER_LOADED = 'USER_LOADED';
 export const AUTH_ERROR = 'AUTH_ERROR';
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const REGISTER_FAIL = 'REGISTER_FAIL';
+export const USER_DASHBOARD = 'USER_DASHBOARD';
