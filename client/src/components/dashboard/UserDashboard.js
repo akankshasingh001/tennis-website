86726c54b1355d2f3c9fc114fd85c6e34a6a32f8
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-13 22:00:03 -0400

    Initial setup for user dashboard

--- client/src/components/dashboard/UserDashboard.js
+++ client/src/components/dashboard/UserDashboard.js
@@ -2,7 +2,25 @@ import React, { Component } from 'react';
 
 class UserDashboard extends Component {
   render() {
-    return <div className="ui container">DashBoard</div>;
+    return (
+      <div className="ui container">
+        <div className="ui four column grid">
+          <div className="column">
+            <div className="ui fluid card">
+              <div className="image">
+                <img
+                  src="https://img.favpng.com/13/22/9/tennis-cartoon-play-stock-illustration-png-favpng-H5T2rJXTeJGvx8Gt8fcQt5LfB.jpg"
+                  alt="img"
+                />
+              </div>
+              <div className="content">
+                <a className="header">Akanksha Singh</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
