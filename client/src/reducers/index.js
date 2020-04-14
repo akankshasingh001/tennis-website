21019d7d061e704f7ac0d06aa1bfd80e0ae6bd74
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-14 12:09:47 -0400

    added reducer in combineReducers

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import authenticationUser from './authenticationUser';
+import profile from './profile';
 
 export default combineReducers({
-  auth: authenticationUser
+  auth: authenticationUser,
+  profile
 });
