f3a78143218282df2c67004f073a72bd4c7acf63
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-13 21:27:22 -0400

    Completed Landing Component

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
-  return <div className="landing">Landing Page</div>;
+  return (
+    <div>
+      <section className="landing">
+        <div className="dark-overlay">
+          <div className="landing-inner">
+            <h1 className="x-large">Tennis Junction</h1>
+            <p className="lead">HIT AND NEVER QUIT !</p>
+            <div className="buttons">
+              <Link to="/register" className="ui primary button">
+                Sign Up
+              </Link>
+              <Link to="/login" className="ui green button">
+                Login
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Landing;
