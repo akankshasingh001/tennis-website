671a21b28909610c10173762eb2d04f34dd612d9
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-15 14:47:54 -0400

    added Action creator for account edit

--- client/src/actions/editProfile.js
+++ client/src/actions/editProfile.js
@@ -1,13 +1,31 @@
 import axios from 'axios';
-import { EDIT_PROFILE, AUTH_ERROR } from './types';
+import { EDIT_PROFILE, AUTH_ERROR, EDIT_ACCOUNT } from './types';
 
-const editProfile = () => async dispatch => {
+export const getUserProfile = () => async dispatch => {
   try {
-    const res = await axios.get('api/currentUserDashboard/me');
+    const res = await axios.get('api/getUserProfile/me');
     dispatch({ type: EDIT_PROFILE, payload: res.data });
   } catch (err) {
     dispatch({ type: AUTH_ERROR });
   }
 };
 
-export default editProfile;
+export const editUserAccount = () => async dispatch => {
+  try {
+    const res = await axios.post('api/account');
+    dispatch({ type: EDIT_ACCOUNT, payload: res.data });
+  } catch (err) {
+    dispatch({ type: AUTH_ERROR });
+  }
+};
+
+// export const editUserProfile = ()=> async dispatch=>{
+//   try {
+//     const res = await axios.post('api/profile');
+//     dispatch({})
+//   } catch (err) {
+//     dispatch({type:AUTH_ERROR})
+//   }
+// }
+
+// export default getUserProfile;
