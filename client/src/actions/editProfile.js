4d710f79f1d2162877bd5d5962314deb4ca3cdb4
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-16 17:01:42 -0400

    action creator for edit user account

--- client/src/actions/editProfile.js
+++ client/src/actions/editProfile.js
@@ -1,31 +1,23 @@
 import axios from 'axios';
-import { EDIT_PROFILE, AUTH_ERROR, EDIT_ACCOUNT } from './types';
+import { EDIT_PROFILE, AUTH_ERROR, EDIT_ACCOUNT, USER_PROFILE } from './types';
 
 export const getUserProfile = () => async dispatch => {
   try {
     const res = await axios.get('api/getUserProfile/me');
-    dispatch({ type: EDIT_PROFILE, payload: res.data });
+    dispatch({ type: USER_PROFILE, payload: res.data });
   } catch (err) {
     dispatch({ type: AUTH_ERROR });
   }
 };
 
-export const editUserAccount = () => async dispatch => {
+export const editUserAccount = formData => async dispatch => {
   try {
-    const res = await axios.post('api/account');
+    const config = { headers: { 'Content-Type': 'application/json' } };
+    const res = await axios.post('api/account', formData, config);
+    alert(`Account Updated ${res}`);
     dispatch({ type: EDIT_ACCOUNT, payload: res.data });
+    alert(`Account Updated ${res}`);
   } catch (err) {
     dispatch({ type: AUTH_ERROR });
   }
 };
-
-// export const editUserProfile = ()=> async dispatch=>{
-//   try {
-//     const res = await axios.post('api/profile');
-//     dispatch({})
-//   } catch (err) {
-//     dispatch({type:AUTH_ERROR})
-//   }
-// }
-
-// export default getUserProfile;
