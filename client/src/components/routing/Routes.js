181f1284402839bc67feac785c8f5de076e2a589
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-16 11:09:35 -0400

    Added Private Routes

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -3,13 +3,21 @@ import { Route, Switch } from 'react-router-dom';
 import Login from '../authentication/Login';
 import Register from '../authentication/Register';
 import Dashboard from '../dashboard/UserDashboard';
+import EditDetails from '../dashboard/EditDetails';
+import SearchUser from '../dashboard/SearchUser';
+import NotFound from '../layout/NotFound';
+
+import PrivateRoute from '../routing/PrivateRoute';
 
 const Routes = () => {
   return (
     <Switch>
       <Route path="/login" exact component={Login} />
       <Route path="/register" exact component={Register} />
-      <Route path="/dashboard" exact component={Dashboard} />
+      <PrivateRoute path="/dashboard" exact component={Dashboard} />
+      <PrivateRoute path="/editDetails" exact component={EditDetails} />
+      <PrivateRoute path="/searchPlayer" exact component={SearchUser} />
+      <Route component={NotFound} />
     </Switch>
   );
 };
