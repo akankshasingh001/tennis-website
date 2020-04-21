5bb7e174a6d55fc2ad18a5c16655171e266641e2
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-21 10:23:32 -0400

    logic added to edit profile and remove unwanted variables

--- client/src/components/dashboard/EditProfile.js
+++ client/src/components/dashboard/EditProfile.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { getUserProfile, editUserProfile } from '../../actions/editProfile';
-import Spinner from '../layout/Spinner';
+import CreateForm from './CreateForm';
 
 const EditProfile = ({
   getUserProfile,
@@ -9,6 +9,7 @@ const EditProfile = ({
   profile: { profile }
 }) => {
   const { yearsPlayed, profession, interests, favPro, contactNumber } = profile;
+
   const [formData, setFormData] = useState({
     yearsPlayedform: yearsPlayed,
     professionform: profession,
@@ -25,97 +26,96 @@ const EditProfile = ({
     contactNumberform
   } = formData;
 
-  useEffect(() => {
-    getUserProfile();
-  }, [getUserProfile, editUserProfile]);
-
   const onChange = e =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
+
   const onSubmit = e => {
     e.preventDefault();
-    //console.log(formData);
-    editUserProfile(formData);
+    const profile = {};
+    profile.yearsPlayed = formData.yearsPlayedform;
+    profile.profession = formData.professionform;
+    profile.interests = formData.interestsform;
+    profile.favPro = formData.favProform;
+    profile.contactNumber = formData.contactNumberform;
+    editUserProfile(profile);
   };
 
-  return profile === '' ? (
-    <Spinner />
+  useEffect(() => {
+    getUserProfile();
+  }, [getUserProfile]);
+
+  return !profile ? (
+    <CreateForm />
   ) : (
     <Fragment>
       <div className="ui container">
-        {profile !== '' ? (
-          <form className="ui form" onSubmit={e => onSubmit(e)}>
-            <h1 className="ui dividing header">Edit Profile</h1>
-            <div className="two fields">
-              <div className="field">
-                <label>Years Played</label>
-                <input
-                  type="text"
-                  placeholder="Years Played"
-                  name="yearsPlayedform"
-                  value={yearsPlayedform}
-                  onChange={e => onChange(e)}
-                />
-              </div>
-              <div className="field">
-                <label>Profession</label>
-                <input
-                  type="text"
-                  placeholder="Profession"
-                  name="professionform"
-                  value={professionform}
-                  onChange={e => onChange(e)}
-                />
-              </div>
+        <form className="ui form" onSubmit={e => onSubmit(e)}>
+          <h1 className="ui dividing header">Edit Profile</h1>
+          <div className="two fields">
+            <div className="field">
+              <label>Years Played</label>
+              <input
+                type="text"
+                placeholder="Years Played"
+                name="yearsPlayedform"
+                value={yearsPlayedform}
+                onChange={e => onChange(e)}
+              />
             </div>
-            <div className="two fields">
-              <div className="field">
-                <label>Interests</label>
-                <input
-                  type="text"
-                  placeholder="Interests"
-                  name="interestsform"
-                  value={interestsform}
-                  onChange={e => onChange(e)}
-                />
-              </div>
-              <div className="field">
-                <label>Favrouite Pro</label>
-                <input
-                  type="text"
-                  placeholder="Favrouite Pro"
-                  name="favProform"
-                  value={favProform}
-                  onChange={e => onChange(e)}
-                />
-              </div>
+            <div className="field">
+              <label>Profession</label>
+              <input
+                type="text"
+                placeholder="Profession"
+                name="professionform"
+                value={professionform}
+                onChange={e => onChange(e)}
+              />
             </div>
-            <div className="two fields">
-              <div className="field">
-                <label>Contact Number</label>
-                <input
-                  type="text"
-                  placeholder="Contact Number"
-                  name="contactNumberform"
-                  value={contactNumberform}
-                  onChange={e => onChange(e)}
-                />
-              </div>
-            </div>
-
-            <div className="ui button primary" tabIndex="0">
-              Update Profile
-            </div>
-          </form>
-        ) : (
-          <div>
-            {' '}
-            <Spinner />
           </div>
-        )}
+          <div className="two fields">
+            <div className="field">
+              <label>Interests</label>
+              <input
+                type="text"
+                placeholder="Interests"
+                name="interestsform"
+                value={interestsform}
+                onChange={e => onChange(e)}
+              />
+            </div>
+            <div className="field">
+              <label>Favrouite Pro</label>
+              <input
+                type="text"
+                placeholder="Favrouite Pro"
+                name="favProform"
+                value={favProform}
+                onChange={e => onChange(e)}
+              />
+            </div>
+          </div>
+          <div className="two fields">
+            <div className="field">
+              <label>Contact Number</label>
+              <input
+                type="text"
+                placeholder="Contact Number"
+                name="contactNumberform"
+                value={contactNumberform}
+                onChange={e => onChange(e)}
+              />
+            </div>
+          </div>
+          <input
+            type="submit"
+            className="ui button primary"
+            value="Update Profile"
+          />
+        </form>
       </div>
     </Fragment>
   );
-  // );
 };
 
 const mapStateToProps = state => ({
