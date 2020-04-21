58e38e5014949c9973578f78dcf3ae2bdabe16d4
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-21 10:22:34 -0400

    remove unwanted variable

--- client/src/actions/editProfile.js
+++ client/src/actions/editProfile.js
@@ -14,19 +14,18 @@ export const editUserAccount = formData => async dispatch => {
   try {
     const config = { headers: { 'Content-Type': 'application/json' } };
     const res = await axios.post('api/account', formData, config);
-    //console.log(res.data, formData);
     dispatch({ type: EDIT_ACCOUNT, payload: res.data });
-    //console.log(res.data, formData);
   } catch (err) {
     dispatch({ type: AUTH_ERROR });
   }
 };
 
-export const editUserProfile = formData => async dispatch => {
+export const editUserProfile = (formData, history) => async dispatch => {
   try {
     const config = { headers: { 'Content-Type': 'application/json' } };
     const res = await axios.post('api/profile', formData, config);
     dispatch({ type: EDIT_PROFILE, payload: res.data });
+    history.push('/dashboard');
   } catch (err) {
     dispatch({ type: AUTH_ERROR });
   }
