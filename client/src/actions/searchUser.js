d7406baaa420708e71fcd46b6db958758b88c27b
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-21 13:00:33 -0400

    Logic created for Search user

--- client/src/actions/searchUser.js
+++ client/src/actions/searchUser.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 import { SEARCH_USER, AUTH_ERROR } from './types';
 
-const searchUser = () => async dispatch => {
+const searchUser = formData => async dispatch => {
   try {
-    const res = await axios.get('api/searchUser');
+    const res = await axios.get(
+      `api/searchUser?firstName=${formData.fname}&lastName=${formData.lname}`
+    );
     dispatch({ type: SEARCH_USER, payload: res.data });
   } catch (error) {
     dispatch({ type: AUTH_ERROR });
--- client/src/components/dashboard/SearchUser.js
+++ client/src/components/dashboard/SearchUser.js
@@ -1,23 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import searchUser from '../../actions/searchUser';
 
 const SearchUser = ({ searchUser }) => {
+  const initialValue = { fname: '', lname: '' };
+
+  const [formData, setFormData] = useState(initialValue);
+
+  const onInputChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onFormSubmit = e => {
+    e.preventDefault();
+    searchUser(formData);
+  };
+
   useEffect(() => {
     searchUser();
   }, [searchUser]);
+
   return (
     <div className="ui container">
-      <form className="ui form">
+      <form className="ui form" onSubmit={onFormSubmit}>
         <h4 className="ui dividing header">Search Player</h4>
         <div className="two fields">
           <div className="field">
             <label>First Name</label>
-            <input type="text" placeholder="First Name" name="fname" />
+            <input
+              type="text"
+              placeholder="First Name"
+              name="fname"
+              onChange={onInputChange}
+            />
           </div>
           <div className="field">
             <label>Last Name</label>
-            <input type="text" placeholder="Last Name" name="lname" />
+            <input
+              type="text"
+              placeholder="Last Name"
+              name="lname"
+              onChange={onInputChange}
+            />
           </div>
         </div>
         <input type="submit" className="ui button primary" value="Search" />
