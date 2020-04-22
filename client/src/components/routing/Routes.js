5832dff20419d8950abb52f379ae8bea3c714daf
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-22 16:55:45 -0400

    Added routes for league registration

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -5,6 +5,7 @@ import Register from '../authentication/Register';
 import Dashboard from '../dashboard/UserDashboard';
 import EditDetails from '../dashboard/EditDetails';
 import SearchUser from '../dashboard/SearchUser';
+import LeagueRegistrationDashboard from '../leagueDetails/LeagueRegistrationDashboard';
 import NotFound from '../layout/NotFound';
 
 import PrivateRoute from '../routing/PrivateRoute';
@@ -17,6 +18,11 @@ const Routes = () => {
       <PrivateRoute path="/dashboard" exact component={Dashboard} />
       <PrivateRoute path="/editDetails" exact component={EditDetails} />
       <PrivateRoute path="/searchPlayer" exact component={SearchUser} />
+      <PrivateRoute
+        path="/leagueRegister"
+        exact
+        component={LeagueRegistrationDashboard}
+      />
       <Route component={NotFound} />
     </Switch>
   );
