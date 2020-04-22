7a0c219f2de39489284e670400c84cf8b82c630e
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-22 16:58:00 -0400

    Added LeagueHistory Tabs

---
+++ client/src/components/leagueDetails/LeagueHistoryTabs.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
+class LeagueHistoryTabs extends Component {
+  render() {
+    return (
+      <div className="ui container">
+        <Tabs>
+          <TabList>
+            <Tab>Singles</Tab>
+            <Tab>Doubles</Tab>
+            <Tab>Mixed</Tab>
+          </TabList>
+
+          <TabPanel></TabPanel>
+        </Tabs>
+      </div>
+    );
+  }
+}
+
+export default LeagueHistoryTabs;
