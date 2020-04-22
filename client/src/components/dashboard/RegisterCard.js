c411b3b71365d732ea536af8da1582cb545124f2
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-22 16:57:05 -0400

    Added Register Card Component

---
+++ client/src/components/dashboard/RegisterCard.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class RegisterCard extends Component {
+  render() {
+    return (
+      <div className="ui card">
+        <div className="content">
+          <div className="header">Time left to Join</div>
+        </div>
+        <div className="content">
+          <table>
+            <tr>
+              <td>
+                <h4 className="ui sub header">
+                  <i className="leaf icon green" />
+                  Singles
+                </h4>
+              </td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td>
+                <h4 className="ui sub header">8 Days</h4>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <h4 className="ui sub header">
+                  <i className="leaf icon green" />
+                  Doubles
+                </h4>
+              </td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td>
+                <h4 className="ui sub header">16 Days</h4>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <h4 className="ui sub header">
+                  <i className="snowflake outline icon blue" />
+                  Mixed
+                </h4>
+              </td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td>
+                <h4 className="ui sub header">15 Days</h4>
+              </td>
+            </tr>
+          </table>
+        </div>
+        <div className="extra content">
+          <Link to="/leagueRegister" className="ui button primary">
+            Register
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default RegisterCard;
