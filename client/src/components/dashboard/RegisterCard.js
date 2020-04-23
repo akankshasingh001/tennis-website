e1106f2785f67c87c413a5ae168bff4efb8f5bcf
Akanksha Singh <singhakanksha001@gmail.com>
2020-04-23 15:12:48 -0400

    added tbody

--- client/src/components/dashboard/RegisterCard.js
+++ client/src/components/dashboard/RegisterCard.js
@@ -10,48 +10,50 @@ class RegisterCard extends Component {
         </div>
         <div className="content">
           <table>
-            <tr>
-              <td>
-                <h4 className="ui sub header">
-                  <i className="leaf icon green" />
-                  Singles
-                </h4>
-              </td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td>
-                <h4 className="ui sub header">8 Days</h4>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <h4 className="ui sub header">
-                  <i className="leaf icon green" />
-                  Doubles
-                </h4>
-              </td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td>
-                <h4 className="ui sub header">16 Days</h4>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <h4 className="ui sub header">
-                  <i className="snowflake outline icon blue" />
-                  Mixed
-                </h4>
-              </td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td>
-                <h4 className="ui sub header">15 Days</h4>
-              </td>
-            </tr>
+            <tbody>
+              <tr>
+                <td>
+                  <h4 className="ui sub header">
+                    <i className="leaf icon green" />
+                    Singles
+                  </h4>
+                </td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>
+                  <h4 className="ui sub header">8 Days</h4>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <h4 className="ui sub header">
+                    <i className="leaf icon green" />
+                    Doubles
+                  </h4>
+                </td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>
+                  <h4 className="ui sub header">16 Days</h4>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <h4 className="ui sub header">
+                    <i className="snowflake outline icon blue" />
+                    Mixed
+                  </h4>
+                </td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>
+                  <h4 className="ui sub header">15 Days</h4>
+                </td>
+              </tr>
+            </tbody>
           </table>
         </div>
         <div className="extra content">
